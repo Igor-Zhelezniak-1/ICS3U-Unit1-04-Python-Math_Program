73b31072f11c9ef190bbdf14af8d4457e2309f43
Igor <ihor.zhelezniak@mths.ca>
2021-09-14 16:12:43 +0000

    aded math program

---
+++ math_program.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Igor
+# Created on: Sept 2021
+# This is math program
+
+
+def main():
+    # This is math program
+    print("22 * 33 = {}".format(22 * 33))
+    print("5 + 6 = {}".format(5 + 6))
+
+
+if __name__ == "__main__":
+    main()
